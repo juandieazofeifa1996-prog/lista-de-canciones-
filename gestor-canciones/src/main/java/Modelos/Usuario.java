93dc97325ba2de4f6a5f24510af0a5b915336ce6
juandieazofeifa1996-prog <juan.dieazofeifa1996@gmail.com>
2025-11-16 22:19:26 -0600

    Agregando, Persona y Cancion

---
+++ gestor-canciones/src/main/java/Modelos/Usuario.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+/**
+ *
+ * @author USUARIO1
+ */
+public class Usuario {
+     private final int id;
+    private final String nombre;
+    private final String password;
+
+    public Usuario(int id, String nombre, String password) {
+        this.id = id;
+        this.nombre = nombre;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
+
